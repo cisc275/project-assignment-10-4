66722fe7ec5db1756d5adc9847ffb8c625ba9041
eakresho <eakresho@udel.edu>
2019-04-08 13:01:15 -0400

    more test stuff

--- src/ViewTest.java
+++ src/ViewTest.java
@@ -1,11 +1,58 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 class ViewTest {
-
+	
 	@Test
-	void test() {
+	void updateViewTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void createImageTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void drawImageTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void displayQuizTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void nestAnimationTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void selectBirdTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void updateBirdTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void updateCollidablesTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void updateMiniMapTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void updateBackgroundTest() {
 		fail("Not yet implemented");
 	}
 
