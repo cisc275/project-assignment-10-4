726c64c15b93a5a0ba49873f39943857c657663a
eakresho <eakresho@udel.edu>
2019-04-08 12:53:45 -0400

    added test classes

---
+++ src/ModelTest.java
@@ -0,0 +1,56 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ModelTest {
+
+	@Test
+	void updateTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void updateBirdTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void updateCollidesTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void updateBackgroundTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void updateMiniMapTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void collisionDetectionTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void startQuizTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void endQuizTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void spawnCollidablesTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void enterNestTest() {
+		fail("Not yet implemented");
+	}
+}
