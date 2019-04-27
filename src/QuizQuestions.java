064a75f90a6341802ad37f67b8d38a9171808c6e
LandonJones <lhjones@udel.edu>
2019-04-27 13:35:50 -0400

    some quiz questions

---
+++ src/QuizQuestions.java
@@ -0,0 +1,74 @@
+import java.util.*; 
+import java.io.*;
+public class QuizQuestions {
+	private List<QuizQuestion> unusedQuestions; 
+	private QuizQuestion current;
+
+	public QuizQuestions(String filename) {
+		this.unusedQuestions = new ArrayList<QuizQuestion>();
+		this.current = null; 
+		/**
+		 * Create the questions
+		 */
+		try {
+			BufferedReader reader = new BufferedReader(new 
+			FileReader(filename));
+			String line;
+			int index = 0;
+			String question = ""; 
+			List<String> answers = new ArrayList<String>(); 
+			String correct = ""; 
+			while ((line = reader.readLine()) != null) {
+				switch(index) {
+					case 0: question = line;
+							answers = new ArrayList<String>(); 
+							break; 
+					case 1: answers.add(line); 
+							break; 
+					case 2: answers.add(line); 
+							break; 
+					case 3: answers.add(line); 
+							break; 
+					case 4: answers.add(line); 
+							break; 
+					case 5: correct = line;
+							this.unusedQuestions.add(new QuizQuestion(question, answers, correct));
+							break; 
+				}
+				index = (index + 1) % 7; 
+			}
+			reader.close();
+			} 
+		catch (Exception e) {
+			System.err.format("Exception occurred trying to read '%s'.", filename);
+			e.printStackTrace();
+		}
+			
+		
+	}
+	
+	public QuizQuestion newQuestion() {
+		QuizQuestion result = unusedQuestions.get(0);
+		int index = 1; 
+		while (questionsRemaining() > index) {
+			result = unusedQuestions.get(index); 
+			index++; 
+		}
+		unusedQuestions.remove(result); 
+		current = result; 
+		return result; 
+	}
+	public int questionsRemaining() {
+		return this.unusedQuestions.size(); 
+	}
+	@Override
+	public String toString() {
+		return this.unusedQuestions.toString(); 
+	}
+	public static void main(String[] args) {
+		QuizQuestions qs = new QuizQuestions("images/questions.txt"); 
+		
+		System.out.println(qs); 
+	}
+	
+}
