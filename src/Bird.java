1900129c248fba66cfb2670c1abcc34064fe3ab3
Joshua Davis <jhdavis@udel.edu>
2019-05-05 16:21:03 -0400

    More formatting

--- src/Bird.java
+++ src/Bird.java
@@ -3,16 +3,18 @@ import java.awt.image.BufferedImage;
 import java.io.*;
 
 /**
- * Bird is the sole user controlled GameElement and the focus of the game.  The game is divided into two portions,
- * depending on if the Bird is currently a Northern Harrier or an Osprey
+ * Bird is the sole user controlled GameElement and the focus of the game. The
+ * game is divided into two portions, depending on if the Bird is currently a
+ * Northern Harrier or an Osprey
  * 
  * @author 10-4
  *
  */
 @SuppressWarnings("serial")
-public class Bird extends GameElement implements Serializable{
+public class Bird extends GameElement implements Serializable {
 	/**
-	 * Which bird the player is currently using. Should be "osprey" or "northern harrier".
+	 * Which bird the player is currently using. Should be "osprey" or "northern
+	 * harrier".
 	 */
 	private String birdType;
 	/**
@@ -39,83 +41,86 @@ public class Bird extends GameElement implements Serializable{
 	 * This constant represents how many ticks the bird stays stunned for
 	 */
 	private static final int STUN_TIME_LIMIT = 75;
-    /**
-     * An int for the number of frames that the bird cycles through to animate.
-     */
-    private static final int FRAME_COUNT = 4;
-    /**
-     * Constant for Bird's starting stamina value
-     */
+	/**
+	 * An int for the number of frames that the bird cycles through to animate.
+	 */
+	private static final int FRAME_COUNT = 4;
+	/**
+	 * Constant for Bird's starting stamina value
+	 */
 	private static final int START_STAMINA = 5;
 	/**
 	 * An int representing the birds speed
 	 */
 	private int flyingSpeed;
 	/**
-	 * A boolean value representing whether or not the bird is currently powered up, true indicates that it powered up,
-	 * while false means that it is not.  A bird becomes powered up after consuming an instance of food with a true,
-	 * value for its isSpecialFood attribute
+	 * A boolean value representing whether or not the bird is currently powered up,
+	 * true indicates that it powered up, while false means that it is not. A bird
+	 * becomes powered up after consuming an instance of food with a true, value for
+	 * its isSpecialFood attribute
 	 */
 	private boolean poweredUp;
 	/**
-	 * A boolean value representing whether or not the bird is currently stunned, true indicates that it is stunned, 
-	 * while false means that it is not.  A bird becomes stunned after colliding with an Obstacle
+	 * A boolean value representing whether or not the bird is currently stunned,
+	 * true indicates that it is stunned, while false means that it is not. A bird
+	 * becomes stunned after colliding with an Obstacle
 	 */
 	private boolean isStunned;
 	/**
-	 * An int representing which way the bird is moving.  If direction == 1, then the bird is moving upwards and if 
-	 * direction == -1, the bird is moving downwards.
+	 * An int representing which way the bird is moving. If direction == 1, then the
+	 * bird is moving upwards and if direction == -1, the bird is moving downwards.
 	 */
-    private int direction;
-    /**
-     * An int representing how much energy to bird has, lost over time and when damaged, and gained upon eating.
-     */
-    private int stamina;
+	private int direction;
+	/**
+	 * An int representing how much energy to bird has, lost over time and when
+	 * damaged, and gained upon eating.
+	 */
+	private int stamina;
 	/**
 	 * An array of BufferedImages that stores the different bird image frames
 	 */
-    private BufferedImage[] pics;
-    /**
-     * 
-     */
-    private BufferedImage[] poweredUpPics; 
-    /**
+	private BufferedImage[] pics;
+	/**
+	 * 
+	 */
+	private BufferedImage[] poweredUpPics;
+	/**
 	 * An array of BufferedImages that stores the different stamina bar pictures
 	 */
-    private BufferedImage[] staminaPics;
-    /**
-     * A BufferedImage representing the current stamina bar image
-     */
-    private BufferedImage staminaImage;
-    /**
-     * An int representing the current frame image that is being displayed
-     */
-    private int frameNum;
-    /**
-     * Represents the time stunned upon collision
-     */
-    private int stunTimer;
-    /**
-     * If true, the bird has run out of stamina and the level needs to restart
-     */
-	private boolean fainted = false;
-    
-    /**
-     * An integer representing the time left for the powerup 
-     */
-    private int powerTimer; 
-    
-    /**
-     * 
-     */
-    private static final int POWER_TIMER_LIMIT = 250; 
-    
-    /**
-	 * A constructor which initializes the attributes for the start of the game.  The bird's starting location is set, 
-	 * its direction is set to 0 because it is not moving up or down.  Its xSpeed is set to 0 because it is not moving 
+	private BufferedImage[] staminaPics;
+	/**
+	 * A BufferedImage representing the current stamina bar image
 	 */
-    
-    
+	private BufferedImage staminaImage;
+	/**
+	 * An int representing the current frame image that is being displayed
+	 */
+	private int frameNum;
+	/**
+	 * Represents the time stunned upon collision
+	 */
+	private int stunTimer;
+	/**
+	 * If true, the bird has run out of stamina and the level needs to restart
+	 */
+	private boolean fainted = false;
+
+	/**
+	 * An integer representing the time left for the powerup
+	 */
+	private int powerTimer;
+
+	/**
+	 * 
+	 */
+	private static final int POWER_TIMER_LIMIT = 250;
+
+	/**
+	 * A constructor which initializes the attributes for the start of the game. The
+	 * bird's starting location is set, its direction is set to 0 because it is not
+	 * moving up or down. Its xSpeed is set to 0 because it is not moving
+	 */
+
 	public Bird(int x, int y, int xSpeed, int ySpeed, String imagePath) {
 		super(x, y, xSpeed, ySpeed, imagePath, null);
 		setXloc(START_X_LOC);
@@ -127,26 +132,26 @@ public class Bird extends GameElement implements Serializable{
 		setWidth(BIRD_WIDTH);
 		frameNum = 0;
 		pics = new BufferedImage[FRAME_COUNT];
-		poweredUpPics = new BufferedImage[FRAME_COUNT]; 
+		poweredUpPics = new BufferedImage[FRAME_COUNT];
 		stunTimer = 0;
 		stamina = START_STAMINA;
 		staminaPics = new BufferedImage[6];
 		this.setType(Images.BIRD);
 	}
-	
+
 	/**
-	 * Updates the position of the bird.  If direction == 1 then the bird moves up.  If direction == 0 
-	 * then the bird stays in the same y position.  If direction == -1 then the bird moves down.
-	 * Also handles the stunned status of the bird.
+	 * Updates the position of the bird. If direction == 1 then the bird moves up.
+	 * If direction == 0 then the bird stays in the same y position. If direction ==
+	 * -1 then the bird moves down. Also handles the stunned status of the bird.
 	 */
 	@Override
-	void update(){
+	void update() {
 		if (stamina <= 0) {
 			setFainted(true);
 			System.out.println(fainted);
 		}
 		setXloc(getXloc() + getxSpeed());
-		setYloc(getYloc() + (getySpeed() * (-1) * direction));	
+		setYloc(getYloc() + (getySpeed() * (-1) * direction));
 		if (isStunned) {
 			stunTimer++;
 			if (stunTimer >= STUN_TIME_LIMIT) {
@@ -155,31 +160,31 @@ public class Bird extends GameElement implements Serializable{
 			}
 		}
 		if (poweredUp) {
-			powerTimer++; 
+			powerTimer++;
 			if (powerTimer >= POWER_TIMER_LIMIT) {
-				powerTimer = 0; 
-				poweredUp = false; 
+				powerTimer = 0;
+				poweredUp = false;
 			}
 		}
 	}
 
 	/**
-	 * It changes the frame to be the next frame and returns the old frame to be displayed
+	 * It changes the frame to be the next frame and returns the old frame to be
+	 * displayed
 	 * 
 	 * @return the next frame image of the bird
 	 */
 	public BufferedImage nextFrame() {
 		int currentFrame = frameNum;
-		frameNum = (frameNum+1)%FRAME_COUNT;
+		frameNum = (frameNum + 1) % FRAME_COUNT;
 		if (!poweredUp) {
 			return pics[currentFrame];
-		} 
-		else { 
-			return poweredUpPics[currentFrame]; 
+		} else {
+			return poweredUpPics[currentFrame];
 		}
-		
+
 	}
-	
+
 	/**
 	 * This sets the image of the bird and creates the frames of the bird
 	 * 
@@ -188,79 +193,86 @@ public class Bird extends GameElement implements Serializable{
 	@Override
 	public void setImage(BufferedImage image) {
 		this.image = image;
-		this.width = image.getWidth()/FRAME_COUNT;
+		this.width = image.getWidth() / FRAME_COUNT;
 		this.height = image.getHeight();
-		for(int i = 0; i < FRAME_COUNT; i++)
-    		pics[i] = image.getSubimage(this.width*i, 0, this.width, this.height);
+		for (int i = 0; i < FRAME_COUNT; i++)
+			pics[i] = image.getSubimage(this.width * i, 0, this.width, this.height);
 	}
+
 	public void setPoweredUpPics(BufferedImage image) {
 		for (int i = 0; i < FRAME_COUNT; i++) {
-			poweredUpPics[i] = image.getSubimage(this.width*i, 0, this.width, this.height); 
+			poweredUpPics[i] = image.getSubimage(this.width * i, 0, this.width, this.height);
 		}
 	}
+
 	/**
-	 * Handles adjusting the birds attributes after it becomes powered up by consuming an instance of food with a, 
-	 * true value for its isSpecialFood attribute
+	 * Handles adjusting the birds attributes after it becomes powered up by
+	 * consuming an instance of food with a, true value for its isSpecialFood
+	 * attribute
 	 */
-	void powerUp() {}
+	void powerUp() {
+	}
 
 	@Override
 	public Rectangle getBounds() {
-		return new Rectangle(this.xloc+25,this.yloc+140,this.width-75,60);
+		return new Rectangle(this.xloc + 25, this.yloc + 140, this.width - 75, 60);
 	}
-	
-	
-	/** 
+
+	/**
 	 * @return the flying speed
 	 */
 	public int getFlyingSpeed() {
 		return flyingSpeed;
 	}
-	
+
 	/**
 	 * @param flyingSpeed the speed to fly at
 	 */
 	public void setFlyingSpeed(int flyingSpeed) {
 		this.flyingSpeed = flyingSpeed;
 	}
-	
+
 	/**
 	 * @return true if the bird is powered up.
 	 */
 	public boolean isPoweredUp() {
 		return poweredUp;
 	}
+
 	/**
-	 * @param poweredUp- a boolean which is true if the bird is currently powered up, false otherwise
+	 * @param poweredUp- a boolean which is true if the bird is currently powered
+	 *                   up, false otherwise
 	 */
 	public void setPoweredUp(boolean poweredUp) {
 		this.poweredUp = poweredUp;
 	}
-	
-	/** 
+
+	/**
 	 * @return true if the bird is stunned.
 	 */
 	public boolean isStunned() {
 		return isStunned;
 	}
-	
+
 	/**
-	 * @param isStunned- a boolean which is true if the bird is currently stunned, false otherwise
+	 * @param isStunned- a boolean which is true if the bird is currently stunned,
+	 *                   false otherwise
 	 */
 	public void setStunned(boolean isStunned) {
 		this.isStunned = isStunned;
 	}
-	
+
 	/**
-	 * @return a 1 if the bird is moving up, a 0 if it is not moving up or down and a -1 if it is moving down.
+	 * @return a 1 if the bird is moving up, a 0 if it is not moving up or down and
+	 *         a -1 if it is moving down.
 	 */
 	public int getDirection() {
 		return direction;
 	}
-	
+
 	/**
-	 * @param direction- an int which is 1 if the bird is moving up, a 0 if it is not moving up or down and a -1 
-	 * if it is moving down.
+	 * @param direction- an int which is 1 if the bird is moving up, a 0 if it is
+	 *                   not moving up or down and a -1 if it is moving down.
 	 */
 	public void setDirection(int direction) {
 		this.direction = direction;
@@ -279,7 +291,7 @@ public class Bird extends GameElement implements Serializable{
 	public void setStamina(int stamina) {
 		this.stamina = stamina;
 	}
-	
+
 	/**
 	 * 
 	 * @return the frames of the bird
@@ -295,6 +307,7 @@ public class Bird extends GameElement implements Serializable{
 	public void setPics(BufferedImage[] pics) {
 		this.pics = pics;
 	}
+
 	/**
 	 * 
 	 * @return the staminaPics of the bird
@@ -310,7 +323,7 @@ public class Bird extends GameElement implements Serializable{
 	public void setStaminaPics(BufferedImage[] pics) {
 		this.staminaPics = pics;
 	}
-	
+
 	/**
 	 * 
 	 * @return the frameNum
@@ -318,7 +331,7 @@ public class Bird extends GameElement implements Serializable{
 	public int frameNum() {
 		return this.frameNum;
 	}
-	
+
 	/**
 	 * 
 	 * @param frameNum the frameNum to set
@@ -326,7 +339,7 @@ public class Bird extends GameElement implements Serializable{
 	public void setFrameNum(int frameNum) {
 		this.frameNum = frameNum;
 	}
-	
+
 	/**
 	 * 
 	 * @return the frame count
@@ -334,7 +347,6 @@ public class Bird extends GameElement implements Serializable{
 	public int getFrameCount() {
 		return Bird.FRAME_COUNT;
 	}
-	
 
 	/**
 	 * @return the birdType
@@ -358,7 +370,8 @@ public class Bird extends GameElement implements Serializable{
 	 * Implements the required method from GameElement
 	 * 
 	 * @param Bird the bird of the game
-	 * @return a boolean value of whether there was a collision. Always returns false for Bird
+	 * @return a boolean value of whether there was a collision. Always returns
+	 *         false for Bird
 	 */
 	@Override
 	public boolean collision(Bird bird) {
@@ -367,6 +380,7 @@ public class Bird extends GameElement implements Serializable{
 
 	/**
 	 * Gets the current stamina image
+	 * 
 	 * @return BufferedImage the staminaImage
 	 */
 	public BufferedImage getStaminaImage() {
@@ -395,8 +409,8 @@ public class Bird extends GameElement implements Serializable{
 	}
 
 	public BufferedImage[] getPoweredUpPics() {
-		return this.poweredUpPics; 
-}
+		return this.poweredUpPics;
+	}
 
 	@Override
 	public int getPointValue() {
