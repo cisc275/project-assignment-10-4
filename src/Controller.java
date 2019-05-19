21a8e9d6e82014f04431fcc9e11a0072a6b8b390
Charlie <charlie.hannum@gmail.com>
2019-05-19 12:51:47 -0400

    quiz questions are selected randomly

--- src/Controller.java
+++ src/Controller.java
@@ -165,6 +165,7 @@ public class Controller implements KeyListener, ActionListener, Serializable{
     	};
     	tutorialMode = true;
     	this.executeTutorial();
+    	//view.setPanel("B");
 	}
 	
 	/**
