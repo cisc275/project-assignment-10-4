aeb96a4bbf90e602226d9c3b5b9f98332497019d
eakresho <eakresho@udel.edu>
2019-05-19 16:55:05 -0400

    tutorial

--- src/Controller.java
+++ src/Controller.java
@@ -163,8 +163,8 @@ public class Controller implements KeyListener, ActionListener, Serializable{
 			}
 		};
 
-		//view.setPanel("WP");
-		view.setPanel("B");
+		view.setPanel("WP");
+		//view.setPanel("B");
 		drawAction = new AbstractAction() {
 			public void actionPerformed(ActionEvent e) {
 				if (!model.isQuizMode() && !model.isDoingQuiz() && isGameInProgress && !model.isReachedEnd()) {
